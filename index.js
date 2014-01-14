4cce4ba5c59cf9884f8d4ce3e2e31841f7e89674
Alexander Zonov <alexander.zonov@developonbox.ru>
2014-01-15 02:03:51 +0400

    Add concat and minify

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var es = require('event-stream');
-var gutil = require('gulp-util');
-var htmlmin = require('minimize');
+var path = require('path');
+var fs = require('fs');
 
-module.exports = function(options) {
+var CleanCSS = require('clean-css');
+var uglify = require('uglify-js');
+var htmlmin = require('minimize');
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+module.exports = function (options) {
 	options = options || {};
 	options.jsmin = options.jsmin !== false;
 	options.cssmin = options.cssmin !== false;
@@ -10,66 +16,116 @@ module.exports = function(options) {
 
 	var startReg = /<!--\s*build:(css|js)\s+([^\s]+)\s*-->/gim;
 	var endReg = /<!--\s*endbuild\s*-->/gim;
+	var jsReg = /<\s*script\s+.*src\s*=\s*"([^"]+)".*><\s*\/\s*script\s*>/gi;
+	var cssReg = /<\s*link\s+.*href\s*=\s*"([^"]+)".*>/gi;
+	var mainPath, mainName;
 
-	function processJs(content) {
-		console.log('TODO js:', content);
-		// TODO concat
-		// TODO uglify
+	function concat(content, reg, delimiter) {
+		var paths = [];
+		var buffer = [];
+
+		content
+			.replace(/<!--(?:.|\r|\n)*-->/gim, '')
+			.replace(reg, function (a, b) {
+				paths.push(path.join(mainPath, b));
+			});
+
+		for (var i = 0, l = paths.length; i < l; ++i)
+			buffer.push(fs.readFileSync(paths[i]));
+
+		return buffer.join(delimiter);
 	}
 
-	function processCss(content) {
-		console.log('TODO css:', content);
-		// TODO concat
-		// TODO minify
+	function processJs(content, name) {
+		var str = concat(content, jsReg, ';\n\n');
+
+		if (options.jsmin)
+			str = uglify.minify(str, {fromString: true}).code;
+
+		return new gutil.File({
+			path: name,
+			contents: new Buffer(str)
+		});
+	}
+
+	function processCss(content, name) {
+		var str = concat(content, cssReg, '\n\n');
+
+		if (options.cssmin)
+			str = new CleanCSS({root: mainPath}).minify(str);
+
+		return new gutil.File({
+			path: name,
+			contents: new Buffer(str)
+		});
 	}
 
 	function processHtml(content, callback) {
-		var result = [];
+		var html = [];
+		var files = [];
 		var sections = content.split(endReg);
 
 		for (var i = 0, l = sections.length; i < l; ++i)
 			if (sections[i].match(startReg)) {
 				var section = sections[i].split(startReg);
 
-				result.push(section[0]);
+				html.push(section[0]);
 
 				switch (section[1]) {
 					case 'js':
-						result.push('<script src="' + section[2] + '"></script>');
-						processJs(section[3]);
+						html.push('<script src="' + section[2] + '"></script>');
+						files.push(processJs(section[3], section[2]));
 						break;
 					case 'css':
-						result.push('<link rel="stylesheet" href="' + section[2] + '"/>');
-						processCss(section[3]);
+						html.push('<link rel="stylesheet" href="' + section[2] + '"/>');
+						files.push(processCss(section[3], section[2]));
 						break;
 				}
 			}
-		else
-			result.push(sections[i]);
+			else
+				html.push(sections[i]);
 
 		if (options.htmlmin)
-			new htmlmin().parse(result.join(''), callback);
-		else
-			callback(null, result.join(''));
+			new htmlmin().parse(html.join(''), function(err, data) {
+				files.push(new gutil.File({
+					path: mainName,
+					contents: new Buffer(data)
+				}));
+
+				callback(files);
+			});
+		else {
+			files.push(new gutil.File({
+				path: mainName,
+				contents: new Buffer(html)
+			}));
+
+			callback(files);
+		}
 	}
 
-  function process(file, callback) {
-    var isStream = file.contents && typeof file.contents.on === 'function' && typeof file.contents.pipe === 'function';
-    var isBuffer = file.contents instanceof Buffer;
-
-		if (isStream)
-      callback(new gutil.PluginError('gulp-usemin', 'Streaming not supported'), file);
-		else if (!isBuffer)
-			callback(new gutil.PluginError('gulp-usemin', 'Unknown type of input'), file);
+	return through.obj(function (file, enc, callback) {
+		if (file.isNull()) {
+			this.push(file); // Do nothing if no contents
+			callback();
+		}
+		else if (file.isStream()) {
+			this.emit('error', new PluginError('gulp-usemin', 'Streams are not supported!'));
+			callback();
+		}
 		else {
 			var content = String(file.contents);
-			if (endReg.test(content))
-				processHtml(content, function(error, content) {
-					file.contents = new Buffer(content);
-					callback(error, file);
-				});
-		}
-  }
 
-  return es.map(process);
+			if (endReg.test(content)) {
+				mainPath = file.base;
+				mainName = path.basename(file.path);
+
+				processHtml(content, function(files) {
+					for (var i = 0; i < files.length; ++ i)
+						this.push(files[i]);
+					callback();
+				}.bind(this));
+			}
+		}
+	});
 };
